2a5edecfe0f030abbc41b805271524e7be041e7b
yalfarosullivan@yahoo.com <Yara Alfaro-Sullivan>
2022-10-10 13:51:48 -0700

    added methods

--- mecurySedan.js
+++ mecurySedan.js
@@ -4,7 +4,7 @@ const VehicleModule = require("./vehicleBaseClass").Vehicle
 class Car extends VehicleModule {
     constructor (make, model, year, color, mileage){
     super(make, model, year, color, mileage);
-    this.maxPassangers = 5;
+    this.maxPassengers = 5;
     this.passenger = 0;
     this.numberOfWheels = 4;
     this.maxspeed = 160;
@@ -19,8 +19,34 @@ checkService(){
     }
 }
 
+start() {
+    if (this.fuel > 0){
+        console.log("engine has started");
+        return this.started = true
+    } else { 
+        console.log9("no fuel");
+        return this.started= false;
+    }
+}
+
+loadPassanger(num){
+    if (this.passenger < this.maxPassenger) {
+        if((number + this.passenger) <= this.maxPassenger) {
+            this.passenger = num;
+            return this.passenger;
+        } else {
+            console.log(this.model + " " + this.make + "not have enough space to take all passengers");
+        }
+    } else {
+        console.log(this.model + " " + this.make + "is full");
+    }
+        }
+    }
+
+
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
+
 console.log(v.make)
 
 
